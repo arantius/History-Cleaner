4a10fba5424804c02a9972fcce22a0c967cc863b
Anthony Lieuallen <alieuall@google.com>
2011-05-03 15:38:39 -0400

    don't duplicate patterns

--- content/pref.js
+++ content/pref.js
@@ -15,30 +15,39 @@ function gHistCleanAddPatternStr(aPatternStr) {
   if (!aPatternStr) return;
 
   var patterns = gHistCleanGetPatterns(); // in case it's the first access
-  patterns.push(new RegExp(aPatternStr));
+  var pattern = new RegExp(aPatternStr);
+  Components.utils.reportError(patterns+'\n');
+  Components.utils.reportError(patternsToStrs(patterns)+'\n');
+  Components.utils.reportError(pattern.source+'\n');
+  if (-1 !== patternsToStrs(patterns).indexOf(pattern.source)) return;
+
+  patterns.push(pattern);
   gHistCleanSetPatterns(patterns);
 }
 
 function gHistCleanGetPatterns() {
   if (null == gHistCleanPatterns) {
     var src = gPrefBranch.getCharPref('patterns');
-    gHistCleanPatterns = JSON.parse(src).map(
-        function(pattern) { return new RegExp(pattern); });
+    gHistCleanPatterns = strsToPatterns(JSON.parse(src));
   }
   return gHistCleanPatterns;
 }
 
 function gHistCleanSetPatterns(aPatterns) {
   gHistCleanPatterns = aPatterns;
-  var src = JSON.stringify(gHistCleanPatterns.map(
-      function(pattern) { return pattern.source; }));
+  var src = JSON.stringify(patternsToStrs(gHistCleanPatterns));
   gPrefBranch.setCharPref('patterns', src);
 }
 
 function gHistCleanSetPatternStrs(aPatternStrs) {
-  gHistCleanPatterns = aPatternStrs.map(
-      function(pattern) { return new RegExp(pattern); });
-  var src = JSON.stringify(gHistCleanPatterns.map(
-      function(pattern) { return pattern.source; }));
+  gHistCleanPatterns = strsToPatterns(aPatternStrs);
+  var src = JSON.stringify(patternsToStrs(gHistCleanPatterns));
   gPrefBranch.setCharPref('patterns', src);
+}
+
+function patternsToStrs(patterns) {
+  return patterns.map(function(pattern) { return pattern.source; });
+}
+function strsToPatterns(strs) {
+  return strs.map(function(str) { return new RegExp(str); });
 }
